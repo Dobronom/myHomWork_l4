7af1362e43bf016ef0ec0d15843377c1de559baf
Aleksandr Tkachev <r91tap@gmail.com>
2024-02-02 19:58:47 +0300

    4 tasks

--- myHomWork_lesson4/myHomWork_lesson4/Program.cs
+++ myHomWork_lesson4/myHomWork_lesson4/Program.cs
@@ -1,1 +1,89 @@
-﻿
+﻿using System.Threading.Channels;
+
+/* вывод всех нечётных чисел. от 1 до 100*/
+//int[] arrey = new int[100];
+//int a = 1;
+//for (int i = 0; i < arrey.Length; i++)
+//{
+//    arrey[i] = a++;
+//    if (arrey[i] % 2 != 0)
+//    {
+//        Console.WriteLine(arrey[i]);
+//    }
+//}
+
+/*вывод массива в обратном порядке*/
+//int[] arrey = new int[5];
+//int a = 1;
+//for (int i = 0; i < arrey.Length; i++)
+//{
+//    arrey[i] = a++;
+//}
+//for (int i = arrey.Length - 1; i >= 0; i--)
+//{
+//    Console.Write(arrey[i]);
+//}
+
+/*вывод суммы всех чисел до введённого*/
+//Console.WriteLine("Введите любое целое положительное число.");
+//int number = Convert.ToInt32(Console.ReadLine());
+//int cum = 0;
+//for (int i = 0; i <= number; i++)
+//{
+//    cum += i;
+//}
+//Console.WriteLine($"Сумма всех чисел до введённого = {cum}");
+
+/*вывод значений с ходом в 7*/
+//int a = 0;
+//while (a % 7 == 0 & a < 98)
+//{
+//    a += 7;
+//    Console.WriteLine(a);
+
+//}
+///*тоже самое из массива*/
+//int[] arrey = new int[100];
+//int b = 1;
+//for (int i = 0; i < arrey.Length; i++)
+//{
+//    arrey[i] = b++;
+
+//}
+//int c = 0;
+//while (arrey[c] < arrey.Length)
+//{
+//    c++;
+//    if (arrey[c] % 7 == 0)
+//        Console.WriteLine(arrey[c]);
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Random r = new Random();
+//for (int i = 0; i < arrey.Length; i++)
+//{
+//    arrey[i] =r.Next(1,101); 
+//}
