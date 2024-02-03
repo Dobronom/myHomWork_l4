883eb92540f078e4e4b3b38959bc6b75a5bffc7e
Aleksandr Tkachev <r91tap@gmail.com>
2024-02-03 14:31:07 +0300

    for comments

---
+++ myHomWork_lesson4/ClassLibrary2/HomeWork4Class.cs
@@ -0,0 +1,92 @@
+﻿namespace HomeWork4Library
+{
+    public class HomeWork4Class
+    {
+        public static void PositiveAndNegativeCounter(int[,] arrey)
+        {
+            int PositiveCounter = 0;
+            int NegativeCounter = 0;
+            int zero = 0;
+            for (int i = 0; i < arrey.GetLength(0); i++)
+            {
+                for (int j = 0; j < arrey.GetLength(1); j++)
+                {
+                    if (arrey[i, j] > 0)
+                    {
+                        PositiveCounter++;
+                    }
+                    else if (arrey[i, j] < 0)
+                    {
+                        NegativeCounter++;
+                    }
+                    else
+                    {
+                        zero++;
+                    }
+                }
+            }
+            Console.WriteLine($"Положительных: {PositiveCounter}\n отрецательных: {NegativeCounter}\n с нулевым значением: {zero} ");
+        }
+        public static void sorting(int[,] arrey, char a)
+        {
+            int qwer = 0;
+            int qwer1 = 0;
+            
+            
+            
+                for (int i = 0; i < arrey.GetLength(0); i++)
+           
+                {
+                
+                    for (int j = 0; j < arrey.GetLength(1); j++)
+               
+                    {
+                        for (int c = j + 1; c < arrey.GetLength(1); c++)
+                        {
+                            if (a == '+')
+                            {
+                                if (arrey[i, j] > arrey[i, c])
+                                {
+                                    qwer = arrey[i, j];
+                                    arrey[i, j] = arrey[i, c];
+                                    arrey[i, c] = qwer;
+                                }
+                            }
+                            else
+                            {
+                               if (arrey[i, j] < arrey[i, c])
+                               {
+                                   qwer1 = arrey[i, j];
+                                   arrey[i, j] = arrey[i, c];
+                                   arrey[i, c] = qwer1;
+
+                               }
+                            }
+                        }
+                    }
+                }
+            for (int i = 0; i < arrey.GetLength(0); i++)
+            {
+                for (int j = 0; j < arrey.GetLength(1); j++)
+                {
+
+                    Console.Write(arrey[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public static void inversion(int[,] arrey)
+        {
+            for (int i = 0; i < arrey.GetLength(0); i++)
+            {
+                for (int j = arrey.GetLength(1) - 1; j >= 0; j--)
+                {
+
+                    Console.Write(arrey[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+ 
+}
--- myHomWork_lesson4/HomeWork 4+/Program.cs
+++ myHomWork_lesson4/HomeWork 4+/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using ClassLibrary2;
+using HomeWork4Library;
 Console.WriteLine("Введите длину матрицы");   
 int Matrixlength = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите высоту матрицы");
 int Matrixheight = Convert.ToInt32(Console.ReadLine());
 int[,] arrey = new int[Matrixlength, Matrixheight];
 Random r = new Random();
-
+Console.WriteLine("Ваш массив готов:");
 for (int i = 0; i < Matrixlength; i++)
 {
     for (int j = 0; j < Matrixheight; j++)
@@ -19,71 +19,39 @@ for (int i = 0; i < Matrixlength; i++)
     }
     Console.WriteLine();
 }
-//Счётчик отрецательных и положительных чисел
-Class1 qwe = new Class1();
-qwe.Counter(arrey);
-
-    
-
-//сортировка от меньшего к большему
-int qwer = 0;
-int qwer1 = 0;
-Console.WriteLine(arrey.Length);
-for (int i = 0; i < Matrixlength; i++)
+Console.WriteLine("Чтоб узнать количество положительных и отрецательных значений нажмите \"1\"");
+Console.WriteLine("Чтоб отсортировать от меньшего к большему введите \"2\"");
+Console.WriteLine("Чтоб отсортировать от большего к мегтшему введите \"3\"");
+Console.WriteLine("Чтоб произвести инверцию введите \"4\"");
+int qw = 0;
+while(qw == 0)
 {
-    for (int j = 0; j < Matrixheight; j++)
+    Console.WriteLine("Введите команду от 1 до 4х");
+    int a = Convert.ToInt32(Console.ReadLine());
+    switch (a)
     {
-        for(int c = j + 1;c < arrey.GetLength(1); c++)
-        {
-            if (arrey[i,j] > arrey[i,c])
-            {
-                qwer = arrey[i, j];
-                arrey[i, j] = arrey[i, c];
-                arrey[i, c] = qwer;
-
-            }
-        }
+        case 1:
+            HomeWork4Class.PositiveAndNegativeCounter(arrey);
+            Console.WriteLine();
+            break;
+        case 2:
+            char c = '+';
+            HomeWork4Class.sorting(arrey, c);
+            Console.WriteLine();
+            break;
+        case 3:
+            char b = 'q';
+            HomeWork4Class.sorting(arrey, b);
+            Console.WriteLine();
+            break;
+        case 4:
+            HomeWork4Class.inversion(arrey);
+            Console.WriteLine();
+            break;
     }
-}
-
-
-
-
-for (int i = 0; i < Matrixlength; i++)
-{
-    for (int j = 0; j < Matrixheight; j++)
-    {
-        
-        Console.Write(arrey[i, j] + " ");
-    }
-    Console.WriteLine();
-}
-//сортировка от большего к меньшему.
-for (int i = 0; i < Matrixlength; i++)
-{
-    for (int j = 0; j < Matrixheight; j++)
-    {
-        for (int c = j + 1; c < arrey.GetLength(1); c++)
-        {
-            if (arrey[i, j] < arrey[i, c])
-            {
-                qwer1 = arrey[i, j];
-                arrey[i, j] = arrey[i, c];
-                arrey[i, c] = qwer1;
-
-            }
-        }
-    }
-}
-
-for (int i = 0; i < Matrixlength; i++)
-{
-    for (int j = 0; j < Matrixheight; j++)
-    {
-
-        Console.Write(arrey[i, j] + " ");
-    }
-    Console.WriteLine();
+    Console.WriteLine("чтоб продолжить работу введите 0");
+    Console.WriteLine("Для выхода любую другую цифру");
+    qw = Convert.ToInt32(Console.ReadLine());
 }
 
 
@@ -91,33 +59,3 @@ for (int i = 0; i < Matrixlength; i++)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Random r = new Random();
-//for (int i = 0; i < arrey.Length; i++)
-//{
-//    arrey[i] =r.Next(1,101); 
-//}
